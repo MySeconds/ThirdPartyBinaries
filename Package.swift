2e06d0c540d4ebbb86d72d8bb10f6b33c810df11
paneiOS <pane4846@gmail.com>
2025-04-28 22:53:26 +0900

    [FEAT] Tag 1.0.5

--- Package.swift
+++ Package.swift
@@ -16,12 +16,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ModernRIBs",
-            url:      "https://github.com/MySeconds/ThirdPartyBinaries/releases/download/1.0.4/ModernRIBs.xcframework.zip",
+            url:      "https://github.com/MySeconds/ThirdPartyBinaries/releases/download/1.0.5/ModernRIBs.xcframework.zip",
             checksum: "16100ba904e4f417b8d1f78bb638a079d8d613b3269236b0170361ca48247d7b"
         ),
         .binaryTarget(
             name: "SnapKit",
-            url:      "https://github.com/MySeconds/ThirdPartyBinaries/releases/download/1.0.4/SnapKit.xcframework.zip",
+            url:      "https://github.com/MySeconds/ThirdPartyBinaries/releases/download/1.0.5/SnapKit.xcframework.zip",
             checksum: "284eb9679d3b778568ef8a2b4a50c3bd23820b8bb55e40affe62613cae02fea8"
         )
     ]
